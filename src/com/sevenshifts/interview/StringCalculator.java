b86e797ebd318033cf4d5b77a2d41b34ad98312b
Fabricio Pires Obregon <fabricio@serti.com.br>
2019-05-05 20:37:06 -0600

    Part 3c

--- src/com/sevenshifts/interview/StringCalculator.java
+++ src/com/sevenshifts/interview/StringCalculator.java
@@ -3,29 +3,29 @@ package com.sevenshifts.interview;
 public class StringCalculator {
 
     public static int add(String numbers){
-
-
-        if(numbers.equals("")){
+        if(numbers.isBlank()){
             return 0;
         }else if(testCustomDelimiter(numbers)){
-            return 999;
+            return findCustomDelimiter(numbers);
         }else{
             return addWithCustomDelimiter(numbers, ",");
         }
     }
 
     public static int addWithCustomDelimiter(String numbers, String delimiter){
-        String[]  inputs = numbers.split(delimiter);
+        String[] inputs = numbers.split(delimiter);
         int sumNumbers = 0;
+
+        System.out.println(inputs[0]);
+        System.out.println(inputs[1]);
+        System.out.println(inputs[2]);
+
 
         for (String input: inputs) {
             sumNumbers += Integer.parseInt(input.replace("\n", ""));
         }
         return sumNumbers;
     }
-
-
-
 
 
 
@@ -36,25 +36,31 @@ public class StringCalculator {
         return false;
     }
 
-//    private static int addCustomDelimiter(String string){
-//        String delimiter = "";
-//        boolean state = true;
-//        int d = 2, i = 3;
-//        String subNumbers;
-//
-//        while(state){
-//            delimiter = delimiter + String.valueOf(d);
-//            if(string.charAt(d) == '\n'){
-//                state = false;
-//            }else{
-//                d++;
-//                i++;
-//            }
-//        }
-//
-//
-//
-//    }
+
+
+    private static int findCustomDelimiter(String numbers){
+        String delimiter = "";
+        boolean state = true;
+        int d = 2, i = 3;
+        String subNumbers;
+
+        while(state){
+            delimiter = delimiter + String.valueOf(numbers.charAt(d));
+            if(numbers.charAt(d) == '\n'){
+                state = false;
+            }else{
+                d++;
+                i++;
+            }
+        }
+
+
+        delimiter = "[" + delimiter + "]";
+        subNumbers = numbers.substring(i);
+        System.out.println("subnumber " + subNumbers);
+        System.out.println("delimiter " + delimiter);
+        return addWithCustomDelimiter(subNumbers, delimiter);
+    }
 
 
 }
