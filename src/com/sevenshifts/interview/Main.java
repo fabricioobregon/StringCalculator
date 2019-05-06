a5f0ae54b26ba59203ef973f6ae3ecb751729753
Fabricio Pires Obregon <fabricio@serti.com.br>
2019-05-05 21:05:44 -0600

    Bonus 1 Complete

--- src/com/sevenshifts/interview/Main.java
+++ src/com/sevenshifts/interview/Main.java
@@ -15,6 +15,10 @@ public class Main {
             System.out.println("Test 3c input //;\\n1;3;4 should result 8. Result: " + StringCalculator.add("//;\n1;3;4"));
             System.out.println("Test 3e.i input //$\\n1$2$3 should result 6. Result: " + StringCalculator.add("//$\n1$2$3"));
             System.out.println("Test 3e.ii input //@\\n2@3@8 should result 13. Result: " + StringCalculator.add("//@\n2@3@8"));
+            System.out.println("Test Bonus1 input 2,1001 should result 2. Result: " + StringCalculator.add("2,1001"));
+
+
+
             System.out.println("Test 4 input //@\\n2@3@8 should result Exception. Result: " + StringCalculator.add("//@\n-2@-3@-8"));
         }catch (Exception E){
             System.out.println(E);
--- src/com/sevenshifts/interview/StringCalculator.java
+++ src/com/sevenshifts/interview/StringCalculator.java
@@ -1,61 +1,63 @@
 package com.sevenshifts.interview;
 
-public class StringCalculator{
+public class StringCalculator {
 
-    public static int add(String numbers) throws Exception{
-        if(numbers.isBlank()){
+    public static int add(String numbers) throws Exception {
+        if (numbers.isBlank()) {
             return 0;
-        }else if(testCustomDelimiter(numbers)){
+        } else if (testCustomDelimiter(numbers)) {
             return findCustomDelimiter(numbers);
-        }else{
+        } else {
             return addWithCustomDelimiter(numbers, ",");
         }
     }
 
-    public static int addWithCustomDelimiter(String numbers, String delimiter) throws Exception{
+    public static int addWithCustomDelimiter(String numbers, String delimiter) throws Exception {
         String[] inputs = numbers.split(delimiter);
         int sumNumbers = 0;
         int inputToInt;
         String negativeNotAllowed = "";
 
 
-        for (String input: inputs) {
+        for (String input : inputs) {
             inputToInt = Integer.parseInt(input.replace("\n", ""));
-            if (inputToInt < 0){
-                negativeNotAllowed  = negativeNotAllowed + input.replace("\n", "") + " ";
+            if (inputToInt < 0) {
+                negativeNotAllowed = negativeNotAllowed + input.replace("\n", "") + " ";
+            } else {
+
+                if (inputToInt < 1001) {
+                    sumNumbers += inputToInt;
+                }
             }
-            sumNumbers += inputToInt;
         }
 
-        if (negativeNotAllowed.equals("")){
-            return  sumNumbers;
-        }else{
+        if (negativeNotAllowed.equals("")) {
+            return sumNumbers;
+        } else {
             throw new Exception("Negatives not allowed:  " + negativeNotAllowed);
         }
     }
 
 
-
-    private static boolean testCustomDelimiter(String numbers){
-        if(numbers.charAt(0) == '/' && numbers.charAt(1) == '/'){
+    private static boolean testCustomDelimiter(String numbers) {
+        if (numbers.charAt(0) == '/' && numbers.charAt(1) == '/') {
             return true;
         }
         return false;
     }
 
 
-
-    private static int findCustomDelimiter(String numbers) throws Exception{
+    private static int findCustomDelimiter(String numbers) throws Exception {
         String delimiter = "";
         boolean state = true;
         int d = 2, i = 3;
         String subNumbers;
 
-        while(state){
+        while (state) {
             delimiter = delimiter + String.valueOf(numbers.charAt(d));
-            if(numbers.charAt(d) == '\n'){
+            if (numbers.charAt(d) == '\n') {
                 state = false;
-            }else{
+            } else {
                 d++;
                 i++;
             }
