0485e8d4c7643d62f74917d0547be2a9a5bcb5ba
Fabricio Pires Obregon <fabricio@serti.com.br>
2019-05-05 20:13:35 -0600

    Part 3 custom delimiter test

--- src/com/sevenshifts/interview/Main.java
+++ src/com/sevenshifts/interview/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Test 1d input empty should result 0. Result: " + StringCalculator.add(""));
+        System.out.println("Test 1b input empty should result 0. Result: " + StringCalculator.add(""));
         System.out.println("Test 1d input 1,2,5 should result 8. Result: " + StringCalculator.add("1,2,5"));
         System.out.println("Test 1d input 10,20,50 should result 80. Result: " + StringCalculator.add("10,20,50"));
         System.out.println("Test 1d input 11,21,51 should result 83. Result: " + StringCalculator.add("11,21,51"));
         System.out.println("Test 2a input 1\\n,2,3 should result 6. Result: " + StringCalculator.add("1\n,2,3"));
         System.out.println("Test 2b input 1,\\n2,4 should result 7. Result: " + StringCalculator.add("1,\n2,4"));
+        System.out.println("Test 3c input //;\\n1;3;4 should result 8. Result: " + StringCalculator.add("//;\n1;3;4"));
 
     }
 }
--- src/com/sevenshifts/interview/StringCalculator.java
+++ src/com/sevenshifts/interview/StringCalculator.java
@@ -3,17 +3,58 @@ package com.sevenshifts.interview;
 public class StringCalculator {
 
     public static int add(String numbers){
-        numbers = numbers.replace("\n", "");
-        String[]  inputs = numbers.split(",");
-        int sumNumbers = 0;
+
 
         if(numbers.equals("")){
             return 0;
+        }else if(testCustomDelimiter(numbers)){
+            return 999;
         }else{
-            for (String input: inputs) {
-                sumNumbers += Integer.parseInt(input);
-            }
-            return sumNumbers;
+            return addWithCustomDelimiter(numbers, ",");
         }
     }
+
+    public static int addWithCustomDelimiter(String numbers, String delimiter){
+        String[]  inputs = numbers.split(delimiter);
+        int sumNumbers = 0;
+
+        for (String input: inputs) {
+            sumNumbers += Integer.parseInt(input.replace("\n", ""));
+        }
+        return sumNumbers;
+    }
+
+
+
+
+
+
+    private static boolean testCustomDelimiter(String numbers){
+        if(numbers.charAt(0) == '/' && numbers.charAt(1) == '/'){
+            return true;
+        }
+        return false;
+    }
+
+//    private static int addCustomDelimiter(String string){
+//        String delimiter = "";
+//        boolean state = true;
+//        int d = 2, i = 3;
+//        String subNumbers;
+//
+//        while(state){
+//            delimiter = delimiter + String.valueOf(d);
+//            if(string.charAt(d) == '\n'){
+//                state = false;
+//            }else{
+//                d++;
+//                i++;
+//            }
+//        }
+//
+//
+//
+//    }
+
+
 }
